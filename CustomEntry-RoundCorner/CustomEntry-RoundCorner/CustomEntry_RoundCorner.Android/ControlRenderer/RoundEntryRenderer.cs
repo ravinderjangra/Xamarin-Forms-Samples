4d209d76f7145c2245320e241724981c29298df7
Ravinder Jangra <ravinderjangra@live.com>
2017-07-17 13:20:00 +0530

    Custom Renderers - Round Entry Added

---
+++ CustomEntry-RoundCorner/CustomEntry-RoundCorner/CustomEntry_RoundCorner.Android/ControlRenderer/RoundEntryRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using CustomEntry_RoundCorner.CustomControls;
+using Xamarin.Forms.Platform.Android;
+using CustomEntry_RoundCorner.Droid.ControlRenderer;
+using Android.Graphics.Drawables;
+using Android.Text;
+
+[assembly: ExportRenderer(typeof(RoundEntry), typeof(RoundEntryRenderer))]
+namespace CustomEntry_RoundCorner.Droid.ControlRenderer
+{
+	public class RoundEntryRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				GradientDrawable gd = new GradientDrawable();
+				// increase or decrease to changes the corner 
+				gd.SetCornerRadius(30);
+				//Control.Background = gd;
+				Control.Background = Resources.GetDrawable(Resource.Drawable.RoundedEntry);
+				//Control.SetBackgroundDrawable(gd);
+			}
+		}
+	}
+}
---
+++ CustomEntry-RoundCorner/CustomEntry-RoundCorner/CustomEntry_RoundCorner.iOS/ControlRenderers/RoundEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using CustomEntry_RoundCorner.CustomControls;
+using CustomEntry_RoundCorner.iOS.ControlRenderers;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(RoundEntry), typeof(RoundEntryRenderer))]
+namespace CustomEntry_RoundCorner.iOS.ControlRenderers
+{
+	class RoundEntryRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if(Control!=null)
+			{
+				Control.Layer.CornerRadius = 15;
+			}
+		}
+	}
+}
